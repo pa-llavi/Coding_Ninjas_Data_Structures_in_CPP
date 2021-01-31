69f93e567fb782089f23fef5e7f463947f4dc25e
pa-llavi <59191317+pa-llavi@users.noreply.github.com>
2021-01-31 12:03:11 +0530

    Create PrintReverseLL.cpp

---
+++ Linked_List_1/PrintReverseLL.cpp
@@ -0,0 +1,26 @@
+Print reverse LinkedList
+Print a given linked list in reverse order. You need to print the tail first and head last. You can’t change any pointer in the linked list, just print it in reverse order.
+Input format : Linked List elements (separated by space and terminated by -1)
+
+Output format : Linked List elements in reverse order (separated by space)
+
+Sample Input 1 :
+1 2 3 4 5 -1
+Sample Output 1 :
+5 4 3 2 1
+Sample Input 2 :
+1 2 3 -1
+Sample Output 2 :
+3 2 1
+
+
+/***************************************************** SOLUTION **********************************************************************/
+
+
+void printReverse(Node *head){
+	if(head == NULL){
+		return;
+	}
+	printReverse(head->next);
+	cout<<head->data<<" ";
+}
