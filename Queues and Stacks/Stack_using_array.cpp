af3fda4833432315e504759a54641eee0bf57473
pa-llavi <59191317+pa-llavi@users.noreply.github.com>
2021-02-09 14:51:45 +0530

    Create Stack_using_array.cpp

---
+++ Queues and Stacks/Stack_using_array.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class StackUsingArray {
+	int* data;
+	int nextIndex;
+	int capacity;
+	
+	public:
+		StackUsingArray (int totalSize) {
+			data = new int[totalSize];
+			nextIndex = 0;
+			capacity = totalSize;
+		}
+		
+		int size() {
+			return nextIndex;
+		}
+		
+		bool isEmpty() {
+			/*
+			if(nextIndex != 0){
+				return false;
+			}else {
+				return true;
+			}
+			*/
+			return nextIndex == 0;
+		}
+		
+		void push(int element) {
+			if(nextIndex == capacity){
+				cout<<"Stack full!"<<endl;
+				return;
+			}
+			data[nextIndex] = element;
+			nextIndex++;
+		}
+		
+		int pop() {
+			if(isEmpty()) {
+				cout<<"Stack is empty!"<<endl;
+				return INT_MIN;
+			}
+			nextIndex--;
+			return data[nextIndex];
+		}
+		
+		int top(){
+			if(isEmpty()) {
+				cout<<"Stack is empty!"<<endl;
+				return INT_MIN;
+			}
+			return data[nextIndex-1];
+		}
+		
+};
+
+
+int main(){
+	StackUsingArray s(4);
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);
+	
+	cout<<s.top()<<endl;
+	
+	cout<<s.pop()<<endl;
+	cout<<s.pop()<<endl;
+	
+	cout<<s.size()<<endl;
+	
+	cout<<s.isEmpty()<<endl;
+	
+	return 0;
+}
